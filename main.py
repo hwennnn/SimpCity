2db61e5f65c7a3431c54ac2faac82df2e933cda7
hwen <hwendev@gmail.com>
2021-11-15 13:06:30 +0800

    containerize the project

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Driver code (Please delete this once you have worked on the main menu)
+while True:
+    option = input('Please enter an option: ')
+
+    if option == '0':
+        print('Exit game ...')
+        break
+    else:
+        print(f'You have selected an option {option}')
