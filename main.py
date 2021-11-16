b2be8aa603ae0ae4cb660d26616a84fc8354b51e
Glenn <Glenn@LAPTOP-ACFNMGVI>
2021-11-16 10:02:34 +0800

    Added main menu and test

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 
 # Driver code (Please delete this once you have worked on the main menu)
-while True:
-    option = input('Please enter an option: ')
+# while True:
+#     option = input('Please enter an option: ')
 
-    if option == '0':
-        print('Exit game ...')
+#     if option == '0':
+#         print('Exit game ...')
+#         break
+#     else:
+#         print(f'You have selected an option {option}')
+
+from adminFunction import *
+
+while True:
+    displayMainMenu()
+    if(validateMain(promptMainMenu()) == '---- Game Ended----'):
         break
-    else:
-        print(f'You have selected an option {option}')
