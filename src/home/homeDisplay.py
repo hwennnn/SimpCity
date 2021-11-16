6b9a8a19772556ae6d671c16cc16578adb3ca6a3
Glenn <Glenn@LAPTOP-ACFNMGVI>
2021-11-16 16:31:27 +0800

    feat: Added main menu

--- src/home/homeDisplay.py
+++ src/home/homeDisplay.py
@@ -1,6 +1,6 @@
 def displayMainMenu():
 
-    mainMenu = ("""
+    print("""
     Welcome, mayor of Simp City!
     ----------------------------
     1. Start new game
@@ -8,22 +8,16 @@ def displayMainMenu():
 
     0. Exit
     """)
-    print(mainMenu)
-    return mainMenu
 
 def promptMainMenu():
-    displayMainMenu
-    option = input('Please enter an input: ')
-    validateMain(option)
+    return input('Please enter an input: ')
 
 def validateMain(option):
     if option == '0' or option == 0:
         print('---- Game Ended----')
-        return ('---- Game Ended----')
 
     else:
         print(f"You selected option {option}")
-        return(f"You selected option {option}")
 
-if __name__ == '__main__':
-    promptMainMenu()
+# if __name__ == '__main__':
+#     promptMainMenu()
