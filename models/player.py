34866c644b13300dd457a6808396a4438ecfc384
ZazzyDictionary <zac.hong01@gmail.com>
2021-11-18 15:42:09 +0800

    refactor: correct indentation

--- models/player.py
+++ models/player.py
@@ -1,6 +1,5 @@
 from models.grid import Grid
 
-
 class Player:  # Player Class
     def __init__(self):
         self.score = 0
@@ -35,11 +34,11 @@ class Player:  # Player Class
 	pass
 
     def saveGame(self):
-	filename = "test-save"
-	with open(filename,"w") as f: 
-		gridValue = self.grid
-		print(gridValue)
-		f.write(gridValue)
+        filename = "test-save"
+        with open(filename, 'w') as f: 
+            gridValue = self.grid
+            print(gridValue)
+            f.write(gridValue)
 
     def exitToMainMenu(self):
         pass
