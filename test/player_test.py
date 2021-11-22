7a9e0cd542cee536f65c91cfd241006223050f34
hwen <hwendev@gmail.com>
2021-11-22 11:40:51 +0800

    refactor: remove duplicate imports

--- test/player_test.py
+++ test/player_test.py
@@ -1,8 +1,9 @@
 import os
+import sys
+import pytest
 from models.player import Player
 from models.configurations import *
 from pathlib import Path
-import sys
 
 path = str(Path(Path("player_test.py").parent.absolute()).parent.absolute())
 sys.path.insert(0, path)
@@ -10,10 +11,8 @@ sys.path.insert(0, path)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 currentDirectory = Path(dir_path)
 
-from models.player import Player
-import pytest
-
 player_test = Player()
+
 
 def test_displayMainMenu(capfd):
     player_test.displayMainMenu()
@@ -27,17 +26,20 @@ def test_displayMainMenu(capfd):
         0. Exit
         """ in out
 
+
 def test_nonExit(capfd):
     option = "1"
     player_test.validateMain(option)
     out, err = capfd.readouterr()
     assert f"You selected option {option}" in out
 
+
 def test_Exit(capfd):
     option = "0"
     player_test.validateMain(option)
     out, err = capfd.readouterr()
     assert "---- Game Ended----" in out
+
 
 def test_checkFileSaved():
     player_test.saveGame()
