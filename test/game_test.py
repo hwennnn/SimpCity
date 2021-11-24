e6641aa6b8ed3cf73f03c1d0bf39533c24d55615
NotConfident <teeyongteng@gmail.com>
2021-11-24 20:43:09 +0800

    test: added additional tests for the current complete gameplay

--- test/game_test.py
+++ test/game_test.py
@@ -1,3 +1,4 @@
+# Functional Test / Test the whole flow
 from pathlib import Path
 import sys
 path = str(Path(Path('game_test.py').parent.absolute()).parent.absolute())
@@ -5,18 +6,42 @@ sys.path.insert(0, path)
 
 import pytest
 from models.game import Game
+from models.player import Player
 
 game_test = Game()
+player_test = Player()
 
 def test_startNewGame(capfd):
-    game_test.startNew()
+    player_test.validateMain('1')
+    player_test.startNewGame() # Init turns == 1
+    player_test.validateGame('0')
+    player_test.validateMain('0')
+    out, err = capfd.readouterr()
+    assert player_test.turns == 1
+
+# Test the display of a empty grid
+def test_displayGrid(capfd):
+    player_test.displayGrid()
     out, err = capfd.readouterr()
     assert """
+    A     B     C     D
+ +-----+-----+-----+-----+ 
+1|     |     |     |     | 
+ +-----+-----+-----+-----+ 
+2|     |     |     |     | 
+ +-----+-----+-----+-----+ 
+3|     |     |     |     | 
+ +-----+-----+-----+-----+ 
+4|     |     |     |     | 
+ +-----+-----+-----+-----+ 
+""" in out
 
-1.
-2.
-3. See remaining buildings
-4. See current score
-
-5. Save game
-0. Exit to main menu""" in out
+# Test exit when player started the game
+def test_ExitFromGrid(capfd):
+    player_test.validateMain('1')
+    player_test.startNewGame() # Init turns == 1
+    player_test.validateGame('0')
+    player_test.validateMain('0')
+    out, err = capfd.readouterr()
+    assert player_test.turns == 1
+    assert "---- Game Ended----"
