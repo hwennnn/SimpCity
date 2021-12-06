9751ae61d8f00d2a3fe083a3c71975bfa78a5c8e
hwen <hwendev@gmail.com>
2021-12-06 12:09:43 +0800

    test: update unit test for main option menu and exit game with parametrize and pytest raises module

--- test/game_test.py
+++ test/game_test.py
@@ -1,28 +1,31 @@
 # Functional Test / Test the whole flow
+import test.player_test as unit_Player
+from models.player import Player
+from models.game import Game
+import pytest
 from pathlib import Path
 import sys
 path = str(Path(Path('game_test.py').parent.absolute()).parent.absolute())
 sys.path.insert(0, path)
 
-import pytest
-from models.game import Game
-from models.player import Player
-import test.player_test as unit_Player
 
 game_test = Game()
 player_test = Player()
 
-def test_startNewGame(capfd):
-    player_test.validateMain('1')
-    unit_Player.test_displayGrid(capfd) # Reuse unit test from player_test.py
-    assert player_test.turns == 1
 
-# Test exit when player started the game
-def test_ExitFromGrid(capfd):
-    player_test.validateMain('1')
-    player_test.startNewGame() # Init turns == 1
-    player_test.validateGame('0')
-    player_test.validateMain('0')
-    out, err = capfd.readouterr()
-    assert player_test.turns == 1
-    assert "---- Game Ended----"
+# def test_startNewGame(capfd):
+#     player_test.validateMain('1')
+#     unit_Player.test_displayGrid(capfd)  # Reuse unit test from player_test.py
+#     assert player_test.turns == 1
+
+# # Test exit when player started the game
+
+
+# def test_ExitFromGrid(capfd):
+#     player_test.validateMain('1')
+#     player_test.startNewGame()  # Init turns == 1
+#     player_test.validateGame('0')
+#     player_test.validateMain('0')
+#     out, err = capfd.readouterr()
+#     assert player_test.turns == 1
+#     assert "---- Game Ended----"
