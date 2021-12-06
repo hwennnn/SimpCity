64c87edf541e06dd0d5b1e35538a090c5315f0b1
hwen <hwendev@gmail.com>
2021-12-06 12:23:37 +0800

    test: update unit test for game option menu with parametrize and pytest raises module

--- test/player_test.py
+++ test/player_test.py
@@ -17,7 +17,7 @@ player_test = Player()
 
 def test_displayMainMenu(capfd):
     player_test.displayMainMenu()
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert """
 Welcome, mayor of Simp City!
 ----------------------------
@@ -32,7 +32,7 @@ Welcome, mayor of Simp City!
 
 def test_displayGrid(capfd):
     player_test.displayGrid()
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert """
     A     B     C     D
  +-----+-----+-----+-----+ 
@@ -47,29 +47,30 @@ def test_displayGrid(capfd):
 """ in out
 
 
-validOptionMainTestData = \
+validMainOptionTestData = \
     [("1", "You selected option 1"),
      ("2", "You selected option 2")]
 
 
-@pytest.mark.parametrize("option, expectedResult", validOptionMainTestData)
-def test_validOptionMain(capfd, option, expectedResult):
+@pytest.mark.parametrize("option, expectedResult", validMainOptionTestData)
+def test_validMainOption(capfd, option, expectedResult):
     player_test.validateMain(option)
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert expectedResult in out
 
 
-invalidOptionMainTestData = \
+invalidMainOptionTestData = \
     [("3", "Invalid option!"),
      ("4", "Invalid option!"),
      ("10", "Invalid option!"),
-     ("a", "Invalid option!")]
+     ("a", "Invalid option!"),
+     ("abc", "Invalid option!")]
 
 
-@pytest.mark.parametrize("option, expectedResult", invalidOptionMainTestData)
-def test_invalidOptionMain(capfd, option, expectedResult):
+@pytest.mark.parametrize("option, expectedResult", invalidMainOptionTestData)
+def test_invalidMainOption(capfd, option, expectedResult):
     player_test.validateMain(option)
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert expectedResult in out
 
 
@@ -82,31 +83,42 @@ def test_ExitGame(capfd, option, expectedResult):
     with pytest.raises(SystemExit) as e:
         player_test.validateMain(option)
 
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert expectedResult in out
     assert e.type == SystemExit
     assert e.value.code == 0
 
 
-def test_validOptionGame(capfd):
-    option = "2"
+validGameOptionTestData = \
+    [("0", "You selected option 0"),
+     ("1", "You selected option 1"),
+     ("2", "You selected option 2"),
+     ("3", "You selected option 3"),
+     ("4", "You selected option 4"),
+     ("5", "You selected option 5")]
+
+
+@pytest.mark.parametrize("option, expectedResult", validGameOptionTestData)
+def test_validGameOption(capfd, option, expectedResult):
     player_test.validateGame(option)
-    out, err = capfd.readouterr()
-    assert f"You selected option {option}" in out
+    out, _ = capfd.readouterr()
+    assert expectedResult in out
 
 
-def test_invalidOptionGame(capfd):
-    option = "a"
+invalidGameOptionTestData = \
+    [("6", "Invalid option!"),
+     ("7", "Invalid option!"),
+     ("8", "Invalid option!"),
+     ("9", "Invalid option!"),
+     ("10", "Invalid option!"),
+     ("abc", "Invalid option!")]
+
+
+@pytest.mark.parametrize("option, expectedResult", invalidGameOptionTestData)
+def test_invalidGameOption(capfd, option, expectedResult):
     player_test.validateGame(option)
-    out, err = capfd.readouterr()
-    assert f"You selected option {option}" not in out
-
-
-def test_ExitGame11(capfd):
-    option = "0"
-    player_test.validateGame(option)
-    out, err = capfd.readouterr()
-    assert f"You selected option {option}" in out
+    out, _ = capfd.readouterr()
+    assert expectedResult in out
 
 
 def test_checkFileSaved():
