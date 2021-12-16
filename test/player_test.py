f282630ad9d4afadb32a8eb12fc241d7e70c1bfc
hwen <hwendev@gmail.com>
2021-12-16 23:11:12 +0800

    test: added test cases for retrieve two randomised buildings

--- test/player_test.py
+++ test/player_test.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import pytest
+from models.available_buildings import AvailableBuildings
 from models.player import Player
 from models.configurations import *
 from pathlib import Path
@@ -125,3 +126,18 @@ def test_checkFileSaved():
     player_test.saveGame()
     rootDirWithFile = currentDirectory.joinpath(savedGameFilename)
     assert rootDirWithFile.exists()
+
+
+def test_shuffleCurrentAvailableBuildings():
+    available_buildings = player_test.grid.availableBuildings
+    shuffled_buildings = available_buildings.shuffleCurrentAvailableBuildings()
+
+    assert len(shuffled_buildings) >= 2 and len(
+        shuffled_buildings) == len(available_buildings)
+
+
+def test_retriveTwoRandomBuildings():
+    available_buildings = player_test.grid.availableBuildings
+    two_random_buildings = available_buildings.shuffleCurrentAvailableBuildings()
+
+    assert len(two_random_buildings) == 2
