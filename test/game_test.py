f9bdadf2292748dacd0caa5d62b1568c2aa6de89
NotConfident <teeyongteng@gmail.com>
2021-12-20 14:43:09 +0800

    test: Functional Test for Saving Placed Buildings

--- test/game_test.py
+++ test/game_test.py
@@ -1,7 +1,10 @@
-# Functional Test / Test the whole flow
-import test.player_test as unit_Player
+# Functional / Integration Test
+from typing_extensions import Self
 from models.player import Player
 from models.game import Game
+from models.grid import Grid
+from models.enums import Buildings
+from models.configurations import *
 import pytest
 from pathlib import Path
 import sys
@@ -11,21 +14,36 @@ sys.path.insert(0, path)
 
 game_test = Game()
 player_test = Player()
+grid_test = Grid()
 
+# Test Add Building and Saving
+# x = Row
+# y = Column
+def test_SavePlacedBuildings():
+    grid_test.createBuilding(Buildings.BEACH, '1', 'A')
+    gridValue = grid_test.parseGridAsString()
+    with open('testing.txt', 'w+') as f:
+        for row in gridValue:
+            f.writelines(row + "\n")
 
-def test_startNewGame(capfd):
-    player_test.validateMain('1')
-    # Reuse unit test from player_test.py
-    unit_Player.test_displayGrid(capfd)
-    assert player_test.turns == 1
+    row = []
+    grid = []
+    f = open("testing.txt", "r")
+    f = f.read()
 
-# Test exit when player started the game
+    temp = ''
 
-
-def test_ExitFromGrid(capfd):
-    player_test.validateMain('1')
-    player_test.startNewGame()  # Init turns == 1
-    player_test.validateGame('0')
-    assert player_test.turns == 1
-    # Reuse unit test from player_test.py
-    unit_Player.test_ExitGame(capfd, "0", "---- Game Ended----")
+    for i in f:
+        if i == ',':
+            row.append(temp)    # Add a coordinate to a row
+            temp = ''
+            continue    # Ignore ',' and not append
+        if i == '\n':
+            row.append(temp)    # Add the last coordinate for the row as it has '\n' instead of ','
+            grid.append(row)    # Add the row into a list to form a grid
+            row = []
+            temp = ''
+        else:
+            temp += i
+    assert grid[0][0] == 'A1'
+    
