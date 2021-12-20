bc4d26b82135741e47815598689e754260ac2692
NotConfident <teeyongteng@gmail.com>
2021-12-20 14:50:44 +0800

    refactor: Added comments and slight refactor on code placements

--- test/game_test.py
+++ test/game_test.py
@@ -20,30 +20,41 @@ grid_test = Grid()
 # x = Row
 # y = Column
 def test_SavePlacedBuildings():
-    grid_test.createBuilding(Buildings.BEACH, '1', 'A')
-    gridValue = grid_test.parseGridAsString()
+    grid_test.createBuilding(Buildings.BEACH, '1', 'A')     # Building Name, x, y
+
+    # Test saving the current grid (on test) on a separate text file
+    gridValue = grid_test.parseGridAsString()  
     with open('testing.txt', 'w+') as f:
         for row in gridValue:
             f.writelines(row + "\n")
 
-    row = []
-    grid = []
+    # Test if the program saved the correct coordinates
     f = open("testing.txt", "r")
     f = f.read()
 
     temp = ''
-
+    row = []
+    grid = []
+    
+    # Grid will be tested in nested list format
+    # [[None, None, None, None]
+    #  [None, None, None, None]
+    #  [None, None, None, None]
+    #  [None, None, None, None]]
     for i in f:
         if i == ',':
             row.append(temp)    # Add a coordinate to a row
             temp = ''
-            continue    # Ignore ',' and not append
+            continue    # Ignore ',' and not append (it will be 'None,' if not included)
+
         if i == '\n':
             row.append(temp)    # Add the last coordinate for the row as it has '\n' instead of ','
             grid.append(row)    # Add the row into a list to form a grid
             row = []
             temp = ''
+
         else:
             temp += i
-    assert grid[0][0] == 'A1'
+
+    assert grid[0][0] == 'BCH'
     
