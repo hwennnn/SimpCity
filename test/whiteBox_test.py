984497cdb55760c84a6b7a31f9907f1bf07d3dfd
NotConfident <teeyongteng@gmail.com>
2022-01-07 01:54:46 +0800

    test: added whitebox test for placing buildings and saving grid to text file

--- test/whiteBox_test.py
+++ test/whiteBox_test.py
@@ -7,11 +7,47 @@ from models.enums import Buildings
 from models.configurations import *
 from models.available_buildings import AvailableBuildings
 import random
+import os
+from pathlib import Path
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+currentDirectory = Path(dir_path)
+
+def parse_fileToList(f):
+    temp = ''
+    row = []
+    grid = []
+    for i in f:
+        if i == ',':
+            row.append(temp)    # Add a coordinate to a row
+            temp = ''
+            continue    # Ignore ',' and not append (it will be 'None,' if not included)
+
+        if i == '\n':
+            row.append(temp)    # Add the last coordinate for the row as it has '\n' instead of ','
+            grid.append(row)    # Add the row into a list to form a grid
+            row = []
+            temp = ''
+
+        else:
+            temp += i
+    return grid
+
+def saveGridToTextUnderTest(grid):
+    
+    # Test saving the current grid (on test) on a separate text file
+    gridValue = grid.parseGridAsString()  
+    with open('testing.txt', 'w+') as f:
+        for row in gridValue:
+            f.writelines(row + "\n")
+
+    # Test if the program saved the correct coordinates
+    f = open("testing.txt", "r")
+    f = f.read()
+    return f
 
 def test_fillGridWithBuildings(monkeypatch, capfd):
     grid = Grid()
-    game = Game()
-    player = Player()
     availableBuildings = AvailableBuildings()
 
     row = grid.rowCount
@@ -41,3 +77,44 @@ def test_fillGridWithBuildings(monkeypatch, capfd):
     for i in tempList:
         grid.updateGrid(i[1], i[2], i[0])
         assert grid.grid[i[1]][i[2]] is not None
+
+def test_placeAndSave(monkeypatch, capfd):
+    grid = Grid()
+    player = Player()
+    availableBuildings = AvailableBuildings()
+
+    row = grid.rowCount
+    col = grid.colCount
+    total = row * col
+
+    colCounter = 0
+    rowCounter = 0
+    tempList = []
+
+    # Halved for testing purposes. a full grid will not be viable as end of game action will kicks in when loaded
+    for i in range(0, round(total / 2)):
+        # Adds building to grid as long as it has Col has not reached the last alphabet in the grid
+        # A1 -> B1 -> C1 -> D1
+        if colCounter < col:
+            tempList.append([availableBuildings.retriveTwoRandomBuildings()[(random.randint(0,1))], rowCounter, colCounter])
+    
+        # Resets Col back to 0 (A, first alphabet) once last alphabet is reached. D -> A. 
+        # Row is increased by 1. A1 -> A2 (from Row 1 to Row 2)
+        # Adds exclusively one A column only or else first column will be empty if its only increment
+        else:
+            colCounter = 0
+            rowCounter += 1
+            tempList.append([availableBuildings.retriveTwoRandomBuildings()[(random.randint(0,1))], rowCounter, colCounter])
+
+        # Moves to the next Col each loop, A -> B -> C -> D
+        colCounter += 1
+    
+    for i in tempList:
+        grid.updateGrid(i[1], i[2], i[0])
+        assert grid.grid[i[1]][i[2]] is not None
+    
+    # Passing in the state of the current grid
+    saveGridToTextUnderTest(grid)
+    rootDirWithFile = currentDirectory.joinpath("testing.txt")
+    assert rootDirWithFile.exists() == True
+    
