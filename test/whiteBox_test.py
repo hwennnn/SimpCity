4790d816787254da075b7a85aff854b4dd88bb95
NotConfident <teeyongteng@gmail.com>
2022-01-07 01:41:37 +0800

    test: added whitebox test for filling grid with buildings

--- test/whiteBox_test.py
+++ test/whiteBox_test.py
@@ -1,36 +1,43 @@
 # Functional / Integration Test - Black Box Edition
+from models.available_buildings import AvailableBuildings
 from models.player import Player
 from models.game import Game
 from models.grid import Grid
 from models.enums import Buildings
 from models.configurations import *
+from models.available_buildings import AvailableBuildings
 import random
 
-citySize = { 
-    0: 'A',
-    1: 'B',
-    2: 'C',
-    3: 'D',
-    4: 'E',
-    5: 'F',
-    6: 'G',
-    7: 'H',
-    8: 'I',
-    9: 'J',
-    10: 'K',
-    11: 'L',
-    12: 'M',
-    13: 'N',
-    14: 'O',
-    15: 'P',
-    16: 'Q',
-    17: 'R',
-    18: 'S',
-    19: 'T',
-    20: 'U',
-    21: 'V',
-    22: 'W',
-    23: 'X',
-    24: 'Y',
-    25: 'Z',
-}
+def test_fillGridWithBuildings(monkeypatch, capfd):
+    grid = Grid()
+    game = Game()
+    player = Player()
+    availableBuildings = AvailableBuildings()
+
+    row = grid.rowCount
+    col = grid.colCount
+    total = row * col
+
+    colCounter = 0
+    rowCounter = 0
+    tempList = []
+    for i in range(0, total):
+        # Adds building to grid as long as it has Col has not reached the last alphabet in the grid
+        # A1 -> B1 -> C1 -> D1
+        if colCounter < col:
+            tempList.append([availableBuildings.retriveTwoRandomBuildings()[(random.randint(0,1))], rowCounter, colCounter])
+    
+        # Resets Col back to 0 (A, first alphabet) once last alphabet is reached. D -> A. 
+        # Row is increased by 1. A1 -> A2 (from Row 1 to Row 2)
+        # Adds exclusively one A column only or else first column will be empty if its only increment
+        else:
+            colCounter = 0
+            rowCounter += 1
+            tempList.append([availableBuildings.retriveTwoRandomBuildings()[(random.randint(0,1))], rowCounter, colCounter])
+
+        # Moves to the next Col each loop, A -> B -> C -> D
+        colCounter += 1
+    
+    for i in tempList:
+        grid.updateGrid(i[1], i[2], i[0])
+        assert grid.grid[i[1]][i[2]] is not None
