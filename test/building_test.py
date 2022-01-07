5ecafa7a0cc4590d59e03e775ba77a52f2f6988c
hwen <hwendev@gmail.com>
2022-01-08 00:40:40 +0800

    test: add unit tests to calculate monument building score

--- test/building_test.py
+++ test/building_test.py
@@ -100,3 +100,85 @@ def test_calculateParkBuildingScore(buildingPositions, expectedResult):
         gridObject.updateGrid(x, y, buildingName)
 
     assert gridObject.calculateParkBuildingsScore() == expectedResult
+
+
+@pytest.mark.parametrize("option, expectedResult", beachBuildingTestData)
+def test_calculateBeachBuildingScore(option, expectedResult):
+    gridObject = Grid()
+    x, y = option
+
+    beachBuilding = gridObject.createBuilding(Buildings.BEACH.value, x, y)
+    assert beachBuilding.retrieveBuildingScore() == expectedResult
+
+
+monumentBuildingTestData = [
+    (
+        [
+            (0, 1, Buildings.MONUMENT.value)
+        ],
+        1
+    ),
+    (
+        [
+            (0, 1, Buildings.MONUMENT.value),
+            (2, 2, Buildings.MONUMENT.value)
+        ],
+        2
+    ),
+    (
+        [
+            (0, 0, Buildings.MONUMENT.value)  # corner
+        ],
+        2
+    ),
+    (
+        [
+            (0, 0, Buildings.MONUMENT.value),  # corner
+            (1, 1, Buildings.MONUMENT.value),
+        ],
+        3
+    ),
+    (
+        [
+            (0, 0, Buildings.MONUMENT.value),  # corner
+            (0, 3, Buildings.MONUMENT.value),  # corner
+        ],
+        4
+    ),
+    (
+        [
+            (0, 0, Buildings.MONUMENT.value),  # corner
+            (0, 3, Buildings.MONUMENT.value),  # corner
+            (3, 0, Buildings.MONUMENT.value),  # corner
+        ],
+        12
+    ),
+    (
+        [
+            (0, 0, Buildings.MONUMENT.value),  # corner
+            (0, 3, Buildings.MONUMENT.value),  # corner
+            (3, 0, Buildings.MONUMENT.value),  # corner
+            (1, 1, Buildings.MONUMENT.value),
+        ],
+        16
+    ),
+    (
+        [
+            (0, 0, Buildings.MONUMENT.value),  # corner
+            (0, 3, Buildings.MONUMENT.value),  # corner
+            (3, 0, Buildings.MONUMENT.value),  # corner
+            (3, 3, Buildings.MONUMENT.value),  # corner
+        ],
+        16
+    ),
+]
+
+
+@pytest.mark.parametrize("buildingPositions, expectedResult", monumentBuildingTestData)
+def test_calculateMonumentBuildingScore(buildingPositions, expectedResult):
+    gridObject = Grid()
+
+    for x, y, buildingName in buildingPositions:
+        gridObject.updateGrid(x, y, buildingName)
+
+    assert gridObject.retrieveBuildingsScore() == expectedResult
