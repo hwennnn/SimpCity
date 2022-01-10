3f3beb3ebcdaf815c74b2af8867a7fd48795d440
glennpck <glennchia43@gmail.com>
2022-01-10 14:45:09 +0800

    feat: implement logic to calculate house buildings score

--- models/buildings/house.py
+++ models/buildings/house.py
@@ -7,4 +7,29 @@ class House(Building):
         Building.__init__(self, name, x, y)
 
     def retrieveBuildingScore(self, grid):
-        pass
+        result = 0
+
+        # Prepare a list for storing adjacent coordinates and adjacent buildings
+        adjacentPositions = [(self.x + 1, self.y), (self.x - 1, self.y), (self.x, self.y + 1), (self.x, self.y - 1)]
+        adjacentBuildings = []
+
+        for dx, dy in adjacentPositions:
+            if 0 <= dx < 4 and 0 <= dy < 4 and grid[dx][dy] is not None:
+                adjacentBuildings.append(grid[dx][dy])
+
+        #Create a matches case for looping through all adjacent buildings
+        for building in adjacentBuildings:
+
+            match building.getName():
+                #Case of Factory only adds 1 point and breaks the loop
+                case Buildings.FACTORY.value:
+                    result = 1
+                    return result
+                case Buildings.HOUSE.value:
+                    result += 1
+                case Buildings.SHOP.value:
+                    result += 1
+                case Buildings.BEACH.value:
+                    result += 2
+
+        return result
