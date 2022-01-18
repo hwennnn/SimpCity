a70b5edb09ab60335a722bada63d5b74d713d317
NotConfident <teeyongteng@gmail.com>
2022-01-18 17:57:08 +0800

    refactor: added comments

--- test/blackBox_test.py
+++ test/blackBox_test.py
@@ -84,7 +84,7 @@ def buildingPlacements():
 # Type: Intergration
 # Description: Verifying the interaction between selecting city size, selecting building pool and starting game
 # Test Scenario ID: TS_CS_BP_SG_001
-# Test Data: Valid City Size / Valid Building Pool
+# Test Data: Valid City Size, Valid Building Pool - 1,2,3,6,7
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=2:2
 
 def test_TC_CS_BP_SG_001(monkeypatch, capfd):
@@ -106,7 +106,7 @@ def test_TC_CS_BP_SG_001(monkeypatch, capfd):
 # Type: Intergration
 # Description: Verifying the interaction between selecting city size, selecting building pool and starting game
 # Test Scenario ID: TS_CS_BP_SG_001
-# Test Data: Invalid City Size / Valid Building Pool
+# Test Data: Invalid City Size, Valid Building Pool - 1,2,3,6,7
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=3:3
 
 def test_TC_CS_BP_SG_002(monkeypatch, capfd):
@@ -129,7 +129,7 @@ def test_TC_CS_BP_SG_002(monkeypatch, capfd):
 # Type: Intergration
 # Description: Verifying the interaction between selecting city size, selecting building pool and starting game
 # Test Scenario ID: TS_CS_BP_SG_001
-# Test Data: Valid City Size / Invalid Building Pool
+# Test Data: Valid City Size, Invalid Building Pool - 1,2,3,8,10
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=4:4
 
 def test_TC_CS_BP_SG_003(monkeypatch, capfd):
@@ -151,7 +151,7 @@ def test_TC_CS_BP_SG_003(monkeypatch, capfd):
 # Type: Intergration
 # Description: Verifying the interaction between selecting city size, selecting building pool and starting game
 # Test Scenario ID: TS_CS_BP_SG_001
-# Test Data: Invalid City Size / Invalid Building Pool
+# Test Data: Invalid City Size, Invalid Building Pool - 1,2,3,8,10
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=5:5
 
 def test_TC_CS_BP_SG_004(monkeypatch, capfd):
@@ -194,7 +194,7 @@ def test_TC_Grid_Fill_001(monkeypatch, capfd):
 # Type: Functional
 # Description: Verifying the possibility of filling the grid with buildings
 # Test Scenario ID: TS_Grid_Fill_001
-# Test Data: Inalid Coordinates
+# Test Data: Inalid Coordinates - 11, !a, 1a,!/
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=7:7
 
 def test_TC_Grid_Fill_002(monkeypatch, capfd):
@@ -217,7 +217,7 @@ def test_TC_Grid_Fill_002(monkeypatch, capfd):
 # Type: Integration
 # Description: Verifying the interaction between placing buildings and remaining building count
 # Test Scenario ID: TS_PB_BC_001
-# Test Data: Valid coordinates
+# Test Data: Valid coordinates - A1
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=8:8
 
 def test_TC_PB_BC_001(monkeypatch, capfd):
@@ -239,7 +239,7 @@ def test_TC_PB_BC_001(monkeypatch, capfd):
 
 # Type: Integration
 # Description: Verifying the interaction between placing buildings and remaining building count
-# Test Scenario ID: TS_PB_BC_001
+# Test Scenario ID: TS_PB_BC_001 - !1
 # Test Data: Invalid coordinates
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=9:9
 
@@ -289,7 +289,7 @@ def test_TC_PB_DS_001(monkeypatch, capfd):
 # Type: Integration
 # Description: Verifying the interaction between placing buildings and the viewing of game score
 # Test Scenario ID: TS_PB_DS_001
-# Test Data: Invalid coordinates
+# Test Data: Invalid coordinates - 11, 1a, 1a, !/
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=17:17
 
 def test_TC_PB_DS_002(monkeypatch, capfd):
@@ -315,7 +315,7 @@ def test_TC_PB_DS_002(monkeypatch, capfd):
 # Type: Functional
 # Description: Verifying the interaction between exiting from both Game and Main menu
 # Test Scenario ID: TS_Exit_001
-# Test Data: Valid Game Menu Option / Valid Main Menu Option
+# Test Data: Valid Game Menu Option - 1, Valid Main Menu Option - 0, Y, 0
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=22:22
 
 def test_TC_Exit_001(monkeypatch, capfd):
@@ -335,7 +335,7 @@ def test_TC_Exit_001(monkeypatch, capfd):
 # Type: Functional
 # Description: Verifying the interaction between exiting from both Game and Main menu
 # Test Scenario ID: TS_Exit_001
-# Test Data: Valid Game Menu Option / Invalid Main Menu Option
+# Test Data: Valid Game Menu Option - 1, Invalid Main Menu Option 0, Y, 8
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=23:23
 
 def test_TC_Exit_002(monkeypatch, capfd):
@@ -354,7 +354,7 @@ def test_TC_Exit_002(monkeypatch, capfd):
 # Type: Functional
 # Description: Verifying the interaction between exiting from both Game and Main menu
 # Test Scenario ID: TS_Exit_001
-# Test Data: Invalid Game Menu Option
+# Test Data: Valid Main Menu Option - 1, Invalid Game Menu Option - 8
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=24:24
 
 def test_TC_Exit_003(monkeypatch, capfd):
@@ -370,10 +370,10 @@ def test_TC_Exit_003(monkeypatch, capfd):
         pass
 
 
-# Type: Functional
+# Type: Integration
 # Description: Verifying the interaction between starting a new game and exit immediately
 # Test Scenario ID: TS_SG_Exit_001
-# Test Data: Valid option for Main Menu, Valid option for Game Menu and Valid option for Main Menu.
+# Test Data: Valid Option for Main Menu - 1, Valid Option for Game Menu - 0, Y, Valid Option for Main Menu - 0
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=25:25
 
 def test_TC_SG_Exit_001(monkeypatch, capfd):
@@ -388,10 +388,10 @@ def test_TC_SG_Exit_001(monkeypatch, capfd):
     assert e.value.code == 0
 
 
-# Type: Functional
+# Type: Integration
 # Description: Verifying the interaction between starting a new game and exit immediately
 # Test Scenario ID: TS_SG_Exit_001
-# Test Data: Valid option for Main Menu, Valid option for Game Menu and Invalid option for Main Menu.
+# Test Data: Valid Option for Main Menu - 1, Valid Option for Game Menu - 0, Y, Invalid Option for Main Menu - 5
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=26:26
 
 def test_TC_SG_Exit_002(monkeypatch, capfd):
@@ -410,7 +410,7 @@ def test_TC_SG_Exit_002(monkeypatch, capfd):
 # Type: Functional
 # Description: Verifying the interaction between starting a new game and exit immediately
 # Test Scenario ID: TS_SG_Exit_001
-# Test Data: Valid option for Main Menu, Invalid option for Game Menu.
+# Test Data: Valid option for Main Menu - 1, Invalid option for Game Menu - 9
 # Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit?pli=1#gid=768609166&range=27:27
 
 def test_TC_SG_Exit_003(monkeypatch, capfd):
@@ -432,6 +432,12 @@ def test_TC_SG_Exit_003(monkeypatch, capfd):
 ########### UAT Functional Test ##########
 ##########################################
 
+# Type: Functional
+# Description: Verify that user can see main menu
+# Test Scenario ID: UAT_TS_MainMenu_001
+# Test Data: NA
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=2:2
+
 def test_UAT_TC_MainMenu_001(monkeypatch, capfd):
     # Iterates through the list of options that mimics user input
     # Start New Game
@@ -444,9 +450,15 @@ def test_UAT_TC_MainMenu_001(monkeypatch, capfd):
         pass
 
 
+# Type: Functional
+# Description: Verify that user can select building pool
+# Test Scenario ID: UAT_TS_BuildingPool_001
+# Test Data: Valid Building Pool - 1,2,4,6,7
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=4:4
+
 def test_UAT_TC_BuildingPool_001(monkeypatch, capfd):
     # Add City Size Logic
-    # Select Options -> Choose Building Pool, Building Pool Selection,
+    # Select Options -> Choose Building Pool, Building Pool Selection
     tempList = ["3", "1", "1,2,3,6,7"]
 
     # Iterates through the list of options that mimics user input
@@ -460,8 +472,35 @@ def test_UAT_TC_BuildingPool_001(monkeypatch, capfd):
         pass
 
 
-def test_UAT_TC_StartGame_001(monkeypatch, capfd):
+# Type: Functional
+# Description: Verify that user can select building pool
+# Test Scenario ID: UAT_TS_BuildingPool_001
+# Test Data: Invalid Building Pool - 1,2,8,9,10
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=5:5
+
+def test_UAT_TC_BuildingPool_002(monkeypatch, capfd):
     # Add City Size Logic
+    # Select Options -> Choose Building Pool, Building Pool Selection
+    tempList = ["3", "1", "1,2,3,8,10"]
+
+    # Iterates through the list of options that mimics user input
+    try:
+        responses = iter(tempList)
+        monkeypatch.setattr('builtins.input', lambda _: next(responses))
+        import main
+
+    # When list runs out of options, StopIteration error will happen unless game is exited with user inputs
+    except StopIteration as e:
+        pass
+
+
+# Type: Functional
+# Description: Verify that user can a start a game
+# Test Scenario ID: UAT_TS_StartGame_001
+# Test Data: Valid Main Menu Option - 1
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=6:6
+
+def test_UAT_TC_StartGame_001(monkeypatch, capfd):
     # Start Game
     tempList = ["1"]
 
@@ -476,8 +515,13 @@ def test_UAT_TC_StartGame_001(monkeypatch, capfd):
         pass
 
 
+# Type: Functional
+# Description: Verify that user can a start a game
+# Test Scenario ID: UAT_TS_StartGame_001
+# Test Data: Invalid Main Menu Option - 5
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=7:7
+
 def test_UAT_TC_StartGame_002(monkeypatch, capfd):
-    # Add City Size Logic
     # Attempt to Start Game with invalid option
     tempList = ["5"]
 
@@ -491,6 +535,12 @@ def test_UAT_TC_StartGame_002(monkeypatch, capfd):
     except StopIteration as e:
         pass
 
+
+# Type: Functional
+# Description: Verify that user can place building on desired coordinate
+# Test Scenario ID: UAT_TS_PlaceBuilding_001
+# Test Data: Valid Coordinate - A1
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=8:8
 
 def test_UAT_TC_PlaceBuilding_001(monkeypatch, capfd):
     # Add City Size Logic
@@ -508,6 +558,12 @@ def test_UAT_TC_PlaceBuilding_001(monkeypatch, capfd):
         pass
 
 
+# Type: Functional
+# Description: Verify that user can place building on desired coordinate
+# Test Scenario ID: UAT_TS_PlaceBuilding_001
+# Test Data: Invalid Coordinate - A9
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=9:9
+
 def test_UAT_TC_PlaceBuilding_002(monkeypatch, capfd):
     # Add City Size Logic
     # Start Game, Select a random building, Place a building
@@ -523,6 +579,12 @@ def test_UAT_TC_PlaceBuilding_002(monkeypatch, capfd):
     except StopIteration as e:
         pass
 
+
+# Type: Functional
+# Description: Verify that user can see remaining building its count
+# Test Scenario ID: UAT_TS_BuildingCount_001
+# Test Data: Valid Coordinate - A1
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=10:10
 
 def test_UAT_TC_BuildingCount_001(monkeypatch, capfd):
     # Add City Size Logic
@@ -540,6 +602,12 @@ def test_UAT_TC_BuildingCount_001(monkeypatch, capfd):
         pass
 
 
+# Type: Functional
+# Description: Verify that user can see remaining building its count
+# Test Scenario ID: UAT_TS_BuildingCount_001
+# Test Data: Invalid Coordinate - A9
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=11:11
+
 def test_UAT_TC_BuildingCount_002(monkeypatch, capfd):
     # Add City Size Logic
     # Start Game, Select a random building, Place a building with invalid coordinate, Check building count
@@ -556,6 +624,12 @@ def test_UAT_TC_BuildingCount_002(monkeypatch, capfd):
         pass
 
 
+# Type: Functional
+# Description: Verify that user can save game
+# Test Scenario ID: UAT_TS_SaveGame_001
+# Test Data: Valid Coordinate - A1, Valid Game Menu Option - 4
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=12:12
+
 def test_UAT_TC_SaveGame_001(monkeypatch, capfd):
     # Add City Size Logic
     # Start Game, Select a random building, Place a building with invalid coordinate, Save game
@@ -571,11 +645,18 @@ def test_UAT_TC_SaveGame_001(monkeypatch, capfd):
     except StopIteration as e:
         pass
 
+    # Prints out the saved game file for verification
     f = open("saved_game.txt", "r")
     file = f.readlines()
     for i in file:
         print(i)
 
+
+# Type: Functional
+# Description: Verify that game score can be shown
+# Test Scenario ID: UAT_TS_GameScore_001
+# Test Data: Valid Coordinate - A1, Valid Game Menu Option - 3
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=15:15
 
 def test_UAT_TC_GameScore_001(monkeypatch, capfd):
     # Add City Size Logic
@@ -591,6 +672,12 @@ def test_UAT_TC_GameScore_001(monkeypatch, capfd):
     except StopIteration as e:
         pass
 
+
+# Type: Functional
+# Description: Verify that game score can be shown
+# Test Scenario ID: UAT_TS_GameScore_001
+# Test Data: Invalid Coordinate - A9, Valid Game Menu Option - 3
+# Link to Test: https://docs.google.com/spreadsheets/d/1j9zOtrntEV0F12utHqEf2nbwmaoZZrfxYVwqXxvVVEs/edit#gid=1826582149&range=16:16
 
 def test_UAT_TC_GameScore_002(monkeypatch, capfd):
     # Add City Size Logic
