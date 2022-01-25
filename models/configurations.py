08edbed565dca0444a25de18e7ae23f8fb14b290
hwen <hwendev@gmail.com>
2022-01-25 10:51:14 +0800

    docs: add comments for configurations class

--- models/configurations.py
+++ models/configurations.py
@@ -1,2 +1,8 @@
+__docformat__ = "google"
+"""
+This module deals with the configurations used in the SimpCity project.
+"""
+
+# The default filename for the saved game and saved leaderboard.
 savedGameFilename = 'saved_game.txt'
 savedLeaderboardFilename = 'saved_leaderboard.txt'
