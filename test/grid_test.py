d20079497f1ce9754caa000d2a8c1843971e1eaa
zachary <zac.hong01@gmail.com>
2022-01-30 18:47:02 +0800

    test: edit test case

--- test/grid_test.py
+++ test/grid_test.py
@@ -346,8 +346,6 @@ invalidGrids = [
     ,"None,None,None,None\n","None,None,None,None\n","None,None,None,None\n"], False),
     (["*2\n","(4,4)\n","1,2,3,4,5\n","BCH,KKK,None,None\n"
     ,"None,None,None,None\n","None,None,None,None\n","None,None,None,None\n"], False),
-    (["*1\n","(4,4)\n","1,2,3,4,5\n","BCH,None,None,None\n"
-    ,"None,None,None,None\n","None,None,None,None\n","None,None,None,None\n"], False),
     (["*2\n","(0,4)\n","1,2,3,4,5\n","BCH,None,None,None\n"
     ,"None,None,None,None\n","None,None,None,None\n","None,None,None,None\n"], False),
     (["*2\n","(4,0)\n","1,2,3,4,5\n","BCH,None,None,None\n"
